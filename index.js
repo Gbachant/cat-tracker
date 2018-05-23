9cf3cfe251a57b9e635368c4a3502cc7134c61d6
Garrett Bachant <gbachant@gmail.com>
2018-05-23 11:31:45 -0400

    Add firebase config script to index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var config = {
+  apiKey: "AIzaSyBsUz7xIhdKOc4tGnbZk6QYnpRSosTpPk0",
+  authDomain: "cat-tracker-21946.firebaseapp.com",
+  databaseURL: "https://cat-tracker-21946.firebaseio.com",
+  projectId: "cat-tracker-21946",
+  storageBucket: "cat-tracker-21946.appspot.com",
+  messagingSenderId: "627966020887"
+};
+firebase.initializeApp(config);
+
+
 function check(form) {
   if (form.userid.value == "aub" && form.pswrd.value == "123") {
     window.open('aub.html');
